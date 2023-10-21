bac6cc7c3eb5a50580ac04e5ca1cdbbe489dc770
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-10-21 16:03:24 +0200

    mix styles and events

--- 02_Praca_z_drzewem_DOM/blog/main.js
+++ 02_Praca_z_drzewem_DOM/blog/main.js
@@ -10,5 +10,53 @@ const link = document.querySelector(".e-mail a");
 const galleryList = document.querySelectorAll(".gallery-main-photo");
 
 //Zadanie 3
-header.textContent = "Lubię pizzę :)";
-console.log(logo.tagName);
+// header.textContent = "Lubię pizzę :)";
+// console.log(logo.tagName);
+// logo.setAttribute("src", "https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/31/64/52/31645240-4001-50ec-1358-cc0dfda253de/source/512x512bb.jpg");
+// console.log(form.innerHTML);
+// link.setAttribute("href", "mailto:gkowalski@wwsi.edu.pl");
+// const headerClass1 = header.className;
+// const headerClass2 = header.classList;
+// console.log({headerClass1, headerClass2});
+// header.className = "";
+// header.classList.remove("about-blog__content-title");
+// document.querySelector("a").className = headerClass1;
+
+//Zadanie 4
+const newLi = document.createElement("li");
+const newLink = document.createElement("a");
+newLink.setAttribute("href", "#contact");
+newLink.innerText = "kontakt";
+newLi.append(newLink);
+document.querySelector(".navigation-menu ul").append(newLi);
+
+//<li><a href="#contact">kontakt</a></li>
+
+//Zadanie 5
+
+const clone = document.querySelector(".navigation-menu ul li:nth-child(1)").cloneNode(true);
+const clone2 = document.querySelector(".navigation-menu ul li:nth-child(2)").cloneNode(true);
+document.querySelector(".navigation-menu ul").append(clone);
+document.querySelector(".navigation-menu ul").append(clone2);
+
+//Zadanie 6
+
+document.querySelector(".about-me-title").remove();
+
+//Zadanie 1 - style
+header.style.color = "deeppink";
+header.style.fontSize = "40px";
+header.style.textDecoration = "underline";
+
+logo.addEventListener("click", function() {
+    if (this.style.borderColor === "") {
+        this.style.borderColor = "magenta";
+        this.style.borderWidth = "5px";
+        this.style.borderStyle = "solid";
+    }
+    else {
+        this.style.borderColor = "";
+        this.style.borderWidth = "";
+        this.style.borderStyle = "";
+    }
+})
